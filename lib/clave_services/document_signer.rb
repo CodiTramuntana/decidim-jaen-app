2d6fe77d89c26a39e454f5908c305914fcc44ad2
Oliver Valls <199462+tramuntanal@users.noreply.github.com>
2022-07-22 10:34:02 +0200

    Rubocopify

--- lib/clave_services/document_signer.rb
+++ lib/clave_services/document_signer.rb
@@ -2,29 +2,25 @@
 
 module ClaveServices
   class DocumentSigner
-    C14N            = "http://www.w3.org/2001/10/xml-exc-c14n#"
+    C14N = "http://www.w3.org/2001/10/xml-exc-c14n#"
     CANON_ALGORITHM = Nokogiri::XML::XML_C14N_EXCLUSIVE_1_0
-    DSIG            = "http://www.w3.org/2000/09/xmldsig#"
+    DSIG = "http://www.w3.org/2000/09/xmldsig#"
 
-    ENVELOPED_SIG   = "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
-    SIGN_METHOD     = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha512"
-    DIGEST_METHOD   = 'http://www.w3.org/2001/04/xmlenc#sha512'
+    ENVELOPED_SIG = "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
+    SIGN_METHOD = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha512"
+    DIGEST_METHOD = "http://www.w3.org/2001/04/xmlenc#sha512"
 
     def initialize(settings)
       @private_key = settings.get_sp_key
       @certificate = settings.get_sp_cert
-      if @certificate.is_a?(String)
-        @certificate = OpenSSL::X509::Certificate.new(@certificate)
-      end
+      @certificate = OpenSSL::X509::Certificate.new(@certificate) if @certificate.is_a?(String)
     end
 
     def algorithm(element)
       algorithm = element
-      if algorithm.is_a?(REXML::Element)
-        algorithm = element.attribute("Algorithm").value
-      end
+      algorithm = element.attribute("Algorithm").value if algorithm.is_a?(REXML::Element)
 
-      algorithm = algorithm && algorithm =~ /(rsa-)?sha(.*?)$/i && $2.to_i
+      algorithm = algorithm && algorithm =~ /(rsa-)?sha(.*?)$/i && Regexp.last_match(2).to_i
 
       case algorithm
       when 256 then OpenSSL::Digest::SHA256
@@ -37,40 +33,40 @@ module ClaveServices
 
     # Embed signature
     #
-    #<Signature>
-      #<SignedInfo>
-        #<CanonicalizationMethod />
-        #<SignatureMethod />
-        #<Reference>
-           #<Transforms>
-             #<Transform>
-             #<Transform>
-           #<DigestMethod>
-           #<DigestValue>
-        #</Reference>
-      #</SignedInfo>
-      #<SignatureValue />
-      #<KeyInfo />
-    #</Signature>
+    # <Signature>
+    # <SignedInfo>
+    # <CanonicalizationMethod />
+    # <SignatureMethod />
+    # <Reference>
+    # <Transforms>
+    # <Transform>
+    # <Transform>
+    # <DigestMethod>
+    # <DigestValue>
+    # </Reference>
+    # </SignedInfo>
+    # <SignatureValue />
+    # <KeyInfo />
+    # </Signature>
     def sign_document(document)
       noko = Nokogiri::XML(document.to_s) do |config|
         config.options = ::ClaveServices::NOKOGIRI_OPTIONS
       end
 
-      signature_element = REXML::Element.new("ds:Signature").add_namespace('ds', DSIG)
+      signature_element = REXML::Element.new("ds:Signature").add_namespace("ds", DSIG)
       signed_info_element = signature_element.add_element("ds:SignedInfo")
-      signed_info_element.add_element("ds:CanonicalizationMethod", {"Algorithm" => C14N})
-      signed_info_element.add_element("ds:SignatureMethod", {"Algorithm"=>SIGN_METHOD})
+      signed_info_element.add_element("ds:CanonicalizationMethod", { "Algorithm" => C14N })
+      signed_info_element.add_element("ds:SignatureMethod", { "Algorithm"=>SIGN_METHOD })
 
       # Add Reference
-      reference_element = signed_info_element.add_element("ds:Reference", {"URI" => "##{document.uuid}"})
+      reference_element = signed_info_element.add_element("ds:Reference", { "URI" => "##{document.uuid}" })
 
       # Add Transforms
       transforms_element = reference_element.add_element("ds:Transforms")
-      transforms_element.add_element("ds:Transform", {"Algorithm" => ENVELOPED_SIG})
-      c14element = transforms_element.add_element("ds:Transform", {"Algorithm" => C14N})
+      transforms_element.add_element("ds:Transform", { "Algorithm" => ENVELOPED_SIG })
+      transforms_element.add_element("ds:Transform", { "Algorithm" => C14N })
 
-      digest_method_element = reference_element.add_element("ds:DigestMethod", {"Algorithm" => DIGEST_METHOD})
+      digest_method_element = reference_element.add_element("ds:DigestMethod", { "Algorithm" => DIGEST_METHOD })
       canon_doc = noko.canonicalize(CANON_ALGORITHM)
       reference_element.add_element("ds:DigestValue").text = compute_digest(canon_doc, algorithm(digest_method_element))
 
@@ -79,16 +75,16 @@ module ClaveServices
         config.options = ::ClaveServices::NOKOGIRI_OPTIONS
       end
 
-      noko_signed_info_element = noko_sig_element.at_xpath('//ds:Signature/ds:SignedInfo', 'ds' => DSIG)
+      noko_signed_info_element = noko_sig_element.at_xpath("//ds:Signature/ds:SignedInfo", "ds" => DSIG)
       canon_string = noko_signed_info_element.canonicalize(CANON_ALGORITHM)
 
       signature = compute_signature(algorithm(SIGN_METHOD).new, canon_string)
       signature_element.add_element("ds:SignatureValue").text = signature.gsub("\n", "")
 
       # add KeyInfo
-      key_info_element       = signature_element.add_element("ds:KeyInfo")
-      x509_element           = key_info_element.add_element("ds:X509Data")
-      x509_cert_element      = x509_element.add_element("ds:X509Certificate")
+      key_info_element = signature_element.add_element("ds:KeyInfo")
+      x509_element = key_info_element.add_element("ds:X509Data")
+      x509_cert_element = x509_element.add_element("ds:X509Certificate")
       x509_cert_element.text = Base64.strict_encode64(@certificate.to_der).strip
 
       # add the signature
@@ -96,7 +92,8 @@ module ClaveServices
       if issuer_element
         document.root.insert_after issuer_element, signature_element
       else
-        if sp_sso_descriptor = document.elements["/md:EntityDescriptor"]
+        sp_sso_descriptor = document.elements["/md:EntityDescriptor"]
+        if sp_sso_descriptor
           document.root.insert_before sp_sso_descriptor, signature_element
         else
           document.root.add_element(signature_element)
--- lib/omniauth/strategies/clave.rb
+++ lib/omniauth/strategies/clave.rb
@@ -10,7 +10,6 @@ module OmniAuth
     # - https://github.com/omniauth/omniauth/blob/master/lib/omniauth/strategy.rb
     # - https://github.com/omniauth/omniauth-oauth2/blob/master/lib/omniauth/strategies/oauth2.rb
     class Clave < ::OmniAuth::Strategies::SAML
-
       def info
         h= OmniAuth::Strategy.instance_method(:info).bind(self).call
         # last_name contains both surnames (García Rodríguez in Antonio García Rodríguez)
@@ -18,7 +17,6 @@ module OmniAuth
         h["nickname"]= h["name"].gsub(" ", "_").downcase
         h
       end
-
     end
   end
 end
